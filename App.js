b4f3e11c7c7f9d89b681fdfc886247b15f2b6a76
xinchang <tong.xin@northeastern.edu>
2023-09-14 14:11:06 -0700

    added disturction props function

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
-  const name="My Aswesome App"
+  const name="My Aswesome App 001"
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on {name}!</Text>
+      {/* <Text>Open up App.js to start working on {name}!</Text> */}
+      {/* use header component */}
+      {/*V== self close*/}
+      <Header />
+      <Header appName="testApp" another="test"/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <View>
+      <Text>Welcome to {props.appName}</Text>
+    </View>
+  )
+}
+
+// use {}for disctuting props: {appName} will just get appName form props, rather than get props first then get appName
+const HeaderDistructing = ({appName, another}) => {
+    console.log(appName);
+    return (
+      <View>
+        <Text>Welcome to {another}</Text>
+      </View>
+    )
+  }
+
+
+export default Header
